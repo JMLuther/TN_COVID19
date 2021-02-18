b1b89826e5cab275ecaec193e27298984ac71b3d
JMLuther <james.luther@vanderbilt.edu>
2021-02-17 22:20:31 -0600

    setup initial TNCOVID

---
+++ code/Download all data.R
@@ -0,0 +1,43 @@
+## DOWNLOAD DIRECTLY FROM TN DOH SITE ####
+# AGE
+download.file(url = #"https://www.tn.gov/content/dam/tn/health/documents/cedep/novel-coronavirus/datasets/Public-Dataset-Age.xlsx",
+                "https://www.tn.gov/content/dam/tn/health/documents/cedep/novel-coronavirus/datasets/Public-Dataset-Age.XLSX",
+              mode = 'wb',
+              destfile = here::here("data_tndoh/Public-Dataset-Age.xlsx"))
+# tools::md5sum(here::here("data_tndoh/Public-Dataset-Age.xlsx"))
+
+# COUNTY
+download.file(url = #"https://www.tn.gov/content/dam/tn/health/documents/cedep/novel-coronavirus/datasets/Public-Dataset-County-New.xlsx",
+                "https://www.tn.gov/content/dam/tn/health/documents/cedep/novel-coronavirus/datasets/Public-Dataset-County-New.XLSX",
+              mode = 'wb',
+              destfile = here::here("data_tndoh/Public-Dataset-County-New.xlsx"))
+# DAILY STATE TOTALS
+download.file(url = #"https://www.tn.gov/content/dam/tn/health/documents/cedep/novel-coronavirus/datasets/Public-Dataset-Daily-Case-Info.xlsx",
+                "https://www.tn.gov/content/dam/tn/health/documents/cedep/novel-coronavirus/datasets/Public-Dataset-Daily-Case-Info.XLSX",
+              mode = 'wb',
+              destfile = here::here("data_tndoh/Public-Dataset-Daily-Case-Info.xlsx"))
+# RACE-ETHNICITY-SEX
+download.file(url = #"https://www.tn.gov/content/dam/tn/health/documents/cedep/novel-coronavirus/datasets/Public-Dataset-RaceEthSex.xlsx",
+                "https://www.tn.gov/content/dam/tn/health/documents/cedep/novel-coronavirus/datasets/Public-Dataset-RaceEthSex.XLSX",
+              mode = 'wb',
+              destfile = here::here("data_tndoh/Public-Dataset-RaceEthSex.xlsx"))
+
+# TN VACCINATION DATA ####
+## COUNTY
+download.file(url = "https://www.tn.gov/content/dam/tn/health/documents/cedep/novel-coronavirus/datasets/COVID_VACCINE_COUNTY_SUMMARY.XLSX",
+              mode = 'wb',
+              destfile = here::here("data_tndoh/COVID_VACCINE_COUNTY_SUMMARY.XLSX"))
+## STATE
+download.file(url = "https://www.tn.gov/content/dam/tn/health/documents/cedep/novel-coronavirus/datasets/COVID_VACCINE_STATE_SUMMARY.XLSX",
+              mode = 'wb',
+              destfile = here::here("data_tndoh/COVID_VACCINE_STATE_SUMMARY.XLSX"))
+
+## BY AGE (STATE LEVEL)
+download.file(url = "https://www.tn.gov/content/dam/tn/health/documents/cedep/novel-coronavirus/datasets/COVID_VACCINE_AGE_GROUPS.XLSX",
+              mode = 'wb',
+              destfile = here::here("data_tndoh/COVID_VACCINE_AGE_GROUPS.XLSX"))
+
+
+# US DATA FROM THE COVIDTRACKING PROJECT ####
+download.file(url = "https://covidtracking.com/api/v1/states/daily.csv",
+              destfile = here::here("data_covidtracking/daily.csv"))
---
+++ code/Extra plots.R
@@ -0,0 +1,188 @@
+# AGE HEATMAP ####
+max_newcaserate_byage <- max(tndoh_age$ar_new_cases_100k_7davg, na.rm = T)
+ar_newcase_heatmap_100k <-
+  tndoh_age %>% 
+  filter(date >= lubridate::ymd("2020-07-01")) %>% 
+  ggplot(aes(x = date,
+             y = age.f)) +
+  geom_tile(aes(fill = ar_new_cases_100k_7davg,
+                color = ar_new_cases_100k_7davg)) +
+  viridis::scale_fill_viridis(labels = scales::comma,
+                              limits = c(0, max_newcaserate_byage), oob = scales::squish,
+                              option = "plasma") +
+  viridis::scale_color_viridis(labels = scales::comma,
+                               limits = c(0, max_newcaserate_byage), oob = scales::squish,
+                               option = "plasma") +
+  labs(fill = "New Cases/100k/d\n(7dAvg)",
+       color  = "New Cases/100k/d\n(7dAvg)",
+       x = NULL, y = NULL,
+       title = "New Case Rate, by Age (population adjusted)") +
+  scale_x_date(date_breaks = "1 month", date_labels = "%b", expand = c(0, 0)) +
+  theme_minimal() +
+  theme(axis.text.y = element_text(size = 12),
+        panel.grid = element_blank(),
+        legend.position = "bottom",
+        legend.background = element_blank(), 
+        panel.border = element_blank(),
+        legend.title = element_text(size = 10),
+        legend.text =  element_text(size = 8))
+
+# new deaths by age
+max_newdeaths_byage <- max(tndoh_age$ar_new_deaths_100k_7davg, na.rm = T)
+ar_newdeaths_heatmap_100k <-
+  tndoh_age %>% 
+    filter(date >= lubridate::ymd("2020-07-01")) %>% 
+    ggplot(aes(x = date,
+               y = age.f)) +
+    geom_tile(aes(fill = ar_new_deaths_100k_7davg,
+                  color = ar_new_deaths_100k_7davg)) +
+    viridis::scale_fill_viridis(labels = scales::comma,
+                                limits = c(0, max_newdeaths_byage), oob = scales::squish,
+                                option = "plasma") +
+    viridis::scale_color_viridis(labels = scales::comma,
+                                 limits = c(0, max_newdeaths_byage), oob = scales::squish,
+                                 option = "plasma") +
+    labs(fill = "New Deaths/100k/d\n(7dAvg)",
+         color  = "New Deaths/100k/d\n(7dAvg)",
+         x = NULL, y = NULL,
+         title = "Death Rate, by Age (population adjusted)") +
+  scale_x_date(date_breaks = "1 month", date_labels = "%b", expand = c(0, 0)) +
+    theme_minimal() +
+    theme(axis.text.y = element_text(size = 12),
+          panel.grid = element_blank(),
+          legend.position = "bottom",
+          legend.background = element_blank(), 
+          panel.border = element_blank(),
+          legend.title = element_text(size = 10),
+          legend.text =  element_text(size = 8))
+
+# ar_newdeaths_heatmap_100k
+ar_newcase_heatmap_100k
+ar_newdeaths_heatmap_100k
+cowplot::plot_grid(ar_newcase_heatmap_100k,
+                   ar_newdeaths_heatmap_100k, nrow = 1)
+
+## AGE RIDGEPLOT ####
+max_newcaserate_byage <- max(tndoh_age$ar_new_cases_100k_7davg, na.rm = T)
+tndoh_age %>% 
+  filter(date >= lubridate::ymd("2020-04-01")) %>% 
+  ggplot(aes(x = date,
+             y = age.f,
+             height = ar_new_cases_100k_7davg/25,
+             fill = age.f)) +
+    ggridges::geom_ridgeline(alpha = 0.5) +
+    theme(legend.position = "none") +
+    labs(x = NULL, y = NULL,
+       title = "New Case Rate, by Age (cases/100k within age group)") +
+  scale_x_date(date_breaks = "1 month", date_labels = "%b", expand = c(0, 0)) 
+
+tndoh_age %>% 
+  filter(date >= lubridate::ymd("2020-04-01")) %>% 
+  ggplot(aes(x = date,
+             y = age.f,
+             height = ar_new_deaths_100k_7davg,
+             fill = age.f)) +
+  ggridges::geom_ridgeline(alpha = 0.5) +
+  theme(legend.position = "none") +
+  labs(x = NULL, y = NULL,
+       title = "New Deaths, by Age (per 100k within age group)") +
+  scale_x_date(date_breaks = "1 month", date_labels = "%b", expand = c(0, 0))
+
+tndoh_age %>% 
+  filter(date >= lubridate::ymd("2020-04-01")) %>% 
+  ggplot(aes(x = date,
+             y = ar_new_deaths_100k_7davg,
+             fill = age.f)) +
+  geom_area(alpha = 0.5) +
+  theme(legend.position = "none") +
+  labs(x = NULL, y = NULL,
+       title = "Average Daily Deaths, per 100k within age group") +
+  scale_x_date(date_breaks = "1 month", date_labels = "%b", expand = c(0, 0)) +
+  facet_wrap(~age.f, nrow = 1, scales = "free_y") +
+  theme(axis.text.x.bottom = element_text(angle = 90, size = 7))
+
+
+## US HEATMAP WITH CLUSTERING ####
+library(gplots)  
+library(dtwclust)
+max_newcaserate_alldates <- max(covus_currentdata_2163$new_cases_state_7davg_100k, na.rm = T)
+  
+state_order <- 
+  covus_currentdata_2163 %>% 
+  filter(date == max(date)) %>% 
+  mutate(state_bynewcases = reorder(state, new_cases_state_7davg_100k)) %>% 
+  select(state, state_bynewcases)
+
+data_for_usheatmap <- 
+  covus_currentdata_2163 %>% 
+  left_join(state_order, by = "state") %>% 
+  filter(date >= lubridate::ymd("2020-04-01")) 
+
+data_for_usheatmap_wide <-
+  data_for_usheatmap %>% 
+  select(date, state, new_cases_state_7davg_100k) %>% 
+  pivot_wider(names_from = state,
+              values_from = new_cases_state_7davg_100k)
+data_for_usheatmap_wide <- column_to_rownames(data_for_usheatmap_wide, var = "date")
+
+
+dtw_dist <- function(x){dist(x, method="DTW")}  
+gplots::heatmap.2(t(as.matrix(data_for_usheatmap_wide)),
+                  distfun = dtw_dist,
+                  hclustfun = hclust,
+                  dendrogram = "row",
+                  revC = T,
+                  Rowv = T,     # group by STATE
+                  Colv = FALSE, # Don't sort by date
+                  srtRow = NULL,
+                  key = TRUE,
+                  col = gplots::colorpanel(100, low = "white", mid = "white", high = "black"),
+                  main = "COVID19 State Clusters",
+                  # ylab = "State",
+                  # xlab = "Date",
+                  trace = "none",
+                  # scale = "none"
+                  )
+
+library(TSclust)
+ts_matrix_uscovid <-  t(as.matrix(data_for_usheatmap_wide)) # 1 state per row
+# head(data_for_usheatmap_wide) # TSclust can handle dataframes- assumes each column is a series
+dist_ts_dtw <- TSclust::diss(SERIES = data_for_usheatmap_wide, METHOD = "DTWARP") # note the dataframe must be transposed
+
+# VERTICAL
+gplots::heatmap.2(as.matrix(data_for_usheatmap_wide),
+                  distfun = dtw_dist,
+                  hclustfun = hclust,
+                  dendrogram = "column",
+                  revC = T,
+                  Rowv = F,     # group by STATE
+                  Colv = TRUE, # Don't sort by date
+                  # srtRow = NULL,
+                  key = TRUE,
+                  col = bluered(100),
+                  main = "COVID19 State Cluster Analysis",
+                  key.title = "Cases/100k",
+                  key.ylab = NA,
+                  trace = "none")
+# HORIZONTAL
+gplots::heatmap.2(as.matrix(t(data_for_usheatmap_wide)),
+                  distfun = dtw_dist,
+                  hclustfun = hclust,
+                  dendrogram = "row",
+                  revC = F,
+                  Rowv = T,     # group by STATE
+                  Colv = F, # Don't sort by date
+                  # srtRow = NULL,
+                  key = TRUE,
+                  col = bluered(100),
+                  main = "COVID19 State Cluster Analysis",
+                  key.title = "Cases/100k",
+                  key.ylab = NA,
+                  trace = "none")
+
+
+
+tndoh_vax_state
+left_join(tndoh_daily,
+          tndoh_vax_state,
+          by = "date")
